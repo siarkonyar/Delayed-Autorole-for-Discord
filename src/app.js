d7bcddc7fd45a5423cfd11395da7a346c1e7da74
siarkonyar <siarkonyar@gmail.com>
2022-03-20 11:52:40 +0300

    my setTimout

--- src/app.js
+++ src/app.js
@@ -15,11 +15,25 @@ client.on('ready', () => {
     console.log(`Launched as a bot: ${client.user.tag}!`);
 });
 
+function setTimeout_ (fn, delay) {
+    var maxDelay = Math.pow(2,31)-1;
+
+    if (delay > maxDelay) {
+        var args = arguments;
+        args[1] -= maxDelay;
+
+        return setTimeout(function () {
+            setTimeout_.apply(undefined, args);
+        }, maxDelay);
+    }
+
+    return setTimeout.apply(undefined, arguments);
+}
 
 client.on('guildMemberAdd', member => {
-    setTimeout(function() {
+    setTimeout_(function() {
         member.roles.add(member.guild.roles.cache.get(process.env.SERVER_ROLE_ID));
-    }, 900000)
+    }, 300000)
 });
 
 client.login(process.env.DJS_TOKEN);
